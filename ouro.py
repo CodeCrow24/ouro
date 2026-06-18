9e4ccd361475fcce05a8f73e9a52a0f8d951cfdb
Leo <177570865+CodeCrow24@users.noreply.github.com>
2026-06-18 22:47:50 +0200

    terminal execution mode

--- ouro.py
+++ ouro.py
@@ -6,8 +6,11 @@ class OuroError(Exception):
     pass
 
 #global memory variables
-filename = sys.argv[1]
-file_content = open(filename)
+filename = ""
+file_content = ""
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+    file_content = open(filename)
 
 name = ""
 version = ""
@@ -15,17 +18,19 @@ version = ""
 VARS = {}
 
 #Interpreter Constants
-base_statements = ["out", "in", "set", "if", "for", "stop", "verbose"]
+base_statements = ["out", "in", "set", "if", "for", "stop", "verbose", "exit"]
 
 #primary header parsing
 def parse_file(file):
-    global name, version, source
+    global name, version, VARS
 
     lines = file.splitlines()
 
     try:
         name = lines[0]
         version = lines[1]
+        VARS["::pname"] = name
+        VARS["::pversion"] = version
     except IndexError:
         raise OuroError("[CRITICAL 100] Header not found")
 
@@ -34,7 +39,7 @@ def tokenize(line, i):
     valid_tokens = base_statements
     tokens = line.split(" ")
     if tokens[0] not in valid_tokens:
-        raise OuroError(f'[CRITICAL ERROR] Invalid statement on line {i+1}')
+        raise OuroError(f'[CRITICAL ERROR 101] Invalid statement on line {i+1}')
     return tokens
 
 #line handler:
@@ -58,10 +63,28 @@ def handle_line(line, i):
             inbuilt.handle_stop(tokens, line, i)
         case "verbose":
             inbuilt.handle_verbose(tokens, line, i)
+        case "exit":
+            return True
+    return False
+
+def runTerminal():
+    exit = False
+    line = 0
+    while True:
+        exit = handle_line(input(">"), line)
+        line += 1
+        if exit:
+            break
 
 
 #main environment:
 def main(file):
+
+    if not file:
+        print("Running Ouro in terminal:")
+        runTerminal()
+        print("\nexiting ouro terminal")
+        return
 
     #parse for global vars
     content = file.read()
